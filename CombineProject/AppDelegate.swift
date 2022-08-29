0d086d6f135c761b1007b75e7630896c5bd4c798
vladyslav-l11 <88321655+vladyslav-l11@users.noreply.github.com>
2022-08-29 12:10:35 +0300

    init commit

---
+++ CombineProject/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  CombineProject
+//
+//  Created by Vladyslav Lysenko on 29.08.2022.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        true
+    }
+}
